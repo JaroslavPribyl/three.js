068fa451ac7b05db31449eb9be137772fd7af01b
Jaroslav Pribyl <pribyl@cadwork.cz>
2024-12-01 13:40:25 +0100

    x

---
+++ types/src/objects/Group.d.ts
@@ -0,0 +1,8 @@
+import { Object3D } from './../core/Object3D';
+
+export class Group extends Object3D {
+    constructor();
+    type: 'Group';
+    readonly isGroup: true;
+    materialLibraries: any;
+}
